07d196767a4294a35e0794949e89f377185fed0f
Ankit <urankitsahu92@gmail.com>
2022-11-08 10:43:19 +0530

    working in dashboard

--- src/app/components/pages/customer/Dashboard.tsx
+++ src/app/components/pages/customer/Dashboard.tsx
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import Card from "../../common/Card";
-import img from "../../../../assets/images/bank-svgrepo-com.svg";
-import { Link } from "react-router-dom";
+// import img from "../../../../assets/images/bank-svgrepo-com.svg";
+// import { Link } from "react-router-dom";
+import Switch from "../../common/Switch";
 type Props = {};
 
-type State = {};
+type State = {
+  error: boolean;
+};
 
 class Dashboard extends Component<Props, State> {
-  state = {};
-
+  //enableDisable = useState<boolean>(false);
   render() {
     return (
       <>
@@ -41,10 +43,29 @@ class Dashboard extends Component<Props, State> {
             </div>
             <div className="col col-6">
               <div className="col col-12" style={{ height: "36vh" }}>
-                <Card height="100%">1</Card>
-              </div>
-              <div className="col col-12" style={{ height: "36vh" }}>
-                <Card height="100%">1</Card>
+                <Card>
+                  <div className="row centerAlignText">
+                    <div className="col col-12">
+                      Account Number: <b>SB</b>
+                    </div>
+                    <div className="col col-12">
+                      Account Balance: <b>3234</b>
+                    </div>
+                    <div className="col col-6">
+                      <button type="button">View More</button>
+                    </div>
+                    <div className="col col-6">
+                      <Switch
+                        id="chkSwitch"
+                        isChecked={true}
+                        label="Enable/Disable"
+                        onChange={(e) => {
+                          console.log("onchange ", e);
+                        }}
+                      />
+                    </div>
+                  </div>
+                </Card>
               </div>
             </div>
           </div>
